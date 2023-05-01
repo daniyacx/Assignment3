a2be4a7dd34323d085f042a0f237bfc91875ad5a
aubak <aubakirovadania@gmail.com>
2023-05-01 22:23:21 +0600

    enqueue

--- src/MyArrayListQueue.java
+++ src/MyArrayListQueue.java
@@ -1,2 +1,9 @@
+import java.util.NoSuchElementException;
 public class MyArrayListQueue {
+    private static MyArrayList queue = new MyArrayList();
+    MyArrayListQueue() {
+    }
+    public static void enqueue(Object o) {
+        queue.add(o);
+    }
 }
