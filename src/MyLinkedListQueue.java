80251f2efe4550a624fbda152bc11085b2703508
aubak <aubakirovadania@gmail.com>
2023-05-01 23:27:44 +0600

    MyLinkedListQueue update

--- src/MyLinkedListQueue.java
+++ src/MyLinkedListQueue.java
@@ -1,2 +1,11 @@
-public class MyLinkedListQueue {
+import java.util.NoSuchElementException;
+import java.util.LinkedList;
+
+public class MyLinkedListQueue<E> {
+    private LinkedList<E> list;
+
+    public MyLinkedListQueue() {
+
+        list = new LinkedList<E>();
+    }
 }
