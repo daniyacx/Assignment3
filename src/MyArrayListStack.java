1381b4077db7c3bb4c4496bdf81673f6f986d60e
aubak <aubakirovadania@gmail.com>
2023-05-01 22:56:08 +0600

    ArrayListStack pop

--- src/MyArrayListStack.java
+++ src/MyArrayListStack.java
@@ -2,13 +2,23 @@ import java.util.EmptyStackException;
 import java.util.ArrayList;
 public class MyArrayListStack<E> {
     private ArrayList<E> list;
+
     public MyArrayListStack() {
         list = new ArrayList<>();
     }
+
     public void push(E element) {
         list.add(element);
     }
+
     public boolean isEmpty() {
         return list.size() == 0;
+    }
+
+    public E pop() {
+        if (isEmpty()) {
+            throw new EmptyStackException();
+        }
+        return list.remove(0);
     }
 }
