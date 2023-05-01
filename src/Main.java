67db4549804a44f30491840468ad0522e43b06e0
aubak <aubakirovadania@gmail.com>
2023-05-02 00:47:57 +0600

    main LinkedListQueue update

--- src/Main.java
+++ src/Main.java
@@ -25,7 +25,7 @@ public class Main {
         queue.enqueue(4);
         queue.enqueue(5);
         System.out.println("Queue size: " + queue.size());
-        System.out.println("Peek element: " + queue.peek()); */
+        System.out.println("Peek element: " + queue.peek());
         MyArrayListStack<Integer> stack = new MyArrayListStack<>();
 
         // Push some elements onto the stack
@@ -44,6 +44,30 @@ public class Main {
         System.out.println("Is stack empty? " + stack.isEmpty());
 
         // Print the size of the stack
-        System.out.println("Stack size: " + stack.size());
+        System.out.println("Stack size: " + stack.size());*/
+        MyLinkedListQueue<Integer> queue = new MyLinkedListQueue<Integer>();
+
+        // Enqueue some elements
+        queue.enqueue(1);
+        queue.enqueue(2);
+        queue.enqueue(3);
+
+        // Dequeue and print elements until queue is empty
+        while (!queue.isEmpty()) {
+            System.out.println(queue.dequeue());
+        }
+
+        // Try to dequeue from an empty queue (should throw NoSuchElementException)
+        try {
+            queue.dequeue();
+        } catch (NoSuchElementException e) {
+            System.out.println("Caught NoSuchElementException: " + e.getMessage());
+        }
+
+        // Add more elements and check queue size and peek element
+        queue.enqueue(4);
+        queue.enqueue(5);
+        System.out.println("Queue size: " + queue.size());
+        System.out.println("Peek element: " + queue.peek());
     }
 }
