70fe459e040f098639fa2bf977167472e14a8853
aubak <aubakirovadania@gmail.com>
2023-05-01 22:42:03 +0600

    ArrayListStack push

--- src/MyArrayListStack.java
+++ src/MyArrayListStack.java
@@ -1,5 +1,9 @@
 import java.util.EmptyStackException;
 public class MyArrayListStack {
 
-    MyArrayList stack = new MyArrayList();
+    private static MyArrayList stack = new MyArrayList();
+    MyArrayListStack(){}
+    public static void push(Object o){
+        stack.add(o);
+    }
 }
