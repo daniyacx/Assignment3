7193baa8930a79581f6ce5b72fd46eeda666d4e9
aubak <aubakirovadania@gmail.com>
2023-05-02 01:02:54 +0600

    main MyLinkedListStack update

--- src/Main.java
+++ src/Main.java
@@ -45,7 +45,7 @@ public class Main {
 
         // Print the size of the stack
         System.out.println("Stack size: " + stack.size());*/
-        MyLinkedListQueue<Integer> queue = new MyLinkedListQueue<Integer>();
+       /* MyLinkedListQueue<Integer> queue = new MyLinkedListQueue<Integer>();
 
         // Enqueue some elements
         queue.enqueue(1);
@@ -68,6 +68,25 @@ public class Main {
         queue.enqueue(4);
         queue.enqueue(5);
         System.out.println("Queue size: " + queue.size());
-        System.out.println("Peek element: " + queue.peek());
+        System.out.println("Peek element: " + queue.peek());*/
+        MyLinkedListStack<Integer> stack = new MyLinkedListStack<>();
+
+        // Push some elements onto the stack
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+
+        // Print the top element of the stack
+        System.out.println("Top element: " + stack.peek());
+
+        // Pop some elements from the stack
+        System.out.println("Popped element: " + stack.pop());
+        System.out.println("Popped element: " + stack.pop());
+
+        // Check if the stack is empty
+        System.out.println("Is stack empty? " + stack.isEmpty());
+
+        // Print the size of the stack
+        System.out.println("Stack size: " + stack.size());
     }
 }
